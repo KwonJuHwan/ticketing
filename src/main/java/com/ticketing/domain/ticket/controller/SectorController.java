becf2a0c84f1160c4495754a076863c851bd982f
JuHwan Kwon <kjh307ok@gmail.com>
2024-03-16 17:56:58 +0900

    [feat] sectorController 추가

---
+++ src/main/java/com/ticketing/domain/ticket/controller/SectorController.java
@@ -0,0 +1,11 @@
+package com.ticketing.domain.ticket.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/sectors")
+public class SectorController {
+}
